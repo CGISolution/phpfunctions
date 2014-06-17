1f5490f042d888c96b74fee03af48956b290ec26
William Gallios <williamgallios@gmail.com>
2014-06-17 12:11:48 -0400

    added initial class

---
+++ PHPFunctions.php
@@ -0,0 +1,20 @@
+
+/**
+ * Author: William Gallios
+ *
+ * TODO: short description.
+ *
+ * TODO: long description.
+ *
+ */
+class PHPFunctions
+{
+    /**
+     * TODO: short description.
+     *
+     */
+    function __construct ()
+    {
+    
+    }
+}
