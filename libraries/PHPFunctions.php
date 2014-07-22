9471e7e26f57b9e715957f85c9b98dee3fd5b9a0
William Gallios <williamgallios@gmail.com>
2014-07-22 16:49:27 -0400

    added hex2rgb converter

--- libraries/PHPFunctions.php
+++ libraries/PHPFunctions.php
@@ -1015,11 +1015,36 @@ exit;
     
     /**
     * used to redirect
-    * exists to ensure nothing executes after Header Location tag
+    * exits to ensure nothing executes after Header Location tag
     */
     public static function redirect ($location)
     {
 	    header("Location: {$location}");
 	    exit;
+    }
+    
+    /**
+    * converts hex color to RGB
+	* @return object - { r,b,g }
+	*/
+    public static function hex2rgb ($hex)
+    {
+	   $hex = str_replace('#', '', $hex);
+	
+	   if(strlen($hex) == 3)
+	   {
+	      $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+	      $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+	      $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+	      
+	   }
+	   else
+	   {
+	      $r = hexdec(substr($hex,0,2));
+	      $g = hexdec(substr($hex,2,2));
+	      $b = hexdec(substr($hex,4,2));
+	   }
+	   
+	   return array($r, $g, $b);
     }
 }
